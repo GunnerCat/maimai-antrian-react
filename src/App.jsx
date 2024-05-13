31ec38944a455103330eeb0a37b8f600ad3254b4
Pieter Riyanto <ptrryt@gmail.com>
2024-05-14 02:20:09 +0700

    Added Sortable Drag feature in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,97 +1,125 @@
-import { useState, useEffect, useLayoutEffect } from "react";
-import Modal from "./components/Modal/Modal";
-import Cookies from "js-cookie";
-import BemacoLogo from './assets/BemacoLogo.png'
-import "./App.css";
+import { useState, useEffect } from 'react';
+import Modal from './components/Modal/Modal.jsx';
+import BemacoLogo from './assets/BemacoLogo.png';
+import './App.css';
+
+import {
+  DndContext,
+  closestCenter,
+  KeyboardSensor,
+  PointerSensor,
+  useSensor,
+  useSensors,
+} from "@dnd-kit/core";
+import {
+  arrayMove,
+  SortableContext,
+  sortableKeyboardCoordinates,
+  verticalListSortingStrategy,
+} from "@dnd-kit/sortable";
+
+import { SortableItem } from "./components/SortableList/SortableList.jsx";
 
 export default function App() {
   const [names, setNames] = useState([
-    "pieter",
-    "aaron",
-    "racel",
-    "emir",
-    "rehan",
+    'pieter',
+    'aaron',
+    'racel',
+    'emir',
+    'rehan',
   ]);
-  const [isModalOpen, setIsModalOpen] = useState(false);
-  
   const [name, setName] = useState('');
+  const [username, setUsername] = useState(localStorage.getItem("username"));
+  const [showModal, setShowModal] = useState(true);
+
+  const sensors = useSensors(
+    useSensor(PointerSensor),
+    useSensor(KeyboardSensor, {
+      coordinateGetter: sortableKeyboardCoordinates,
+    })
+  );
 
   function handleAddPlayer() {
     setNames([...names, name]);
     setName('')
   }
-  
-  function handleAddPlayers() {
-    setIsModalOpen(!isModalOpen);
-  }
 
+  const [text, setText] = useState('Input Username');
 
   // function handleRemovePlayer() {
   //   const newNames = [...names]; // Create a shallow copy of the names array
   //   newNames.splice(0, 2); // Remove the first two items
   //   setNames(newNames);
   // }
-
-
-  const [actionPerformed, setActionPerformed] = useState(false);
-
+  
   useEffect(() => {
-    // Check if the device has already performed the action
-    if(localStorage.getItem("username")==''){
-      document.getElementById('my_modal_2').showModal()
+    if(username) {
+      setText(`Change Username`);
     }
-    const deviceAction = Cookies.get("deviceAction");
-    if (deviceAction === "true") {
-      setActionPerformed(true);
+    else{
+      setText(`Input Username`);
     }
-  }, []);
-
-  useLayoutEffect(() => { document.body.classList = 'bg-creamy' }, [])
+  }, [username]);
 
   return (
-    <div className="flex flex-col items-center justiy-center min-h-screen max-w-sm mx-auto flex-1">
-      <Modal
-        isOpen={isModalOpen}
-        onClose={() => setIsModalOpen(false)}
-        onQueue={handleAddPlayer}
-      />
-      <header className="flex flex-col item-center mb-5 justify-center">
-          <div className="flex justify-center">
-            <img src={BemacoLogo} className='object-cover justify-bottom'></img>
+    <div className='bg-creamy flex justify-center'>
+      <div className='flex flex-col min-h-screen max-w-sm'>
+        <Modal
+          setUsername={setUsername}
+          showModal={showModal}
+          setShowModal={setShowModal}
+        />
+        <header className='flex flex-col item-center mb-5 justify-center'>
+            <div className='flex justify-center'>
+              <img src={BemacoLogo} className='object-cover justify-bottom'></img>
+            </div>
+            <div className='flex'>Antrian Maimai Hari ini</div>
+            <div className='flex'>Day, DD/MM/YYYY</div>
+        </header>
+        <div className='min-h-50vh mb-5 flex flex-col justify-center'>
+          <div className='mb-5 flex flex-col items-center'>
+            <DndContext
+              sensors={sensors}
+              collisionDetection={closestCenter}
+              onDragEnd={handleDragEnd}
+            >
+              <SortableContext items={names} strategy={verticalListSortingStrategy}>
+                {names.map((name,index ) => (
+                  <SortableItem key={name} name={name} idx={index} itemSize={names.length} />
+                ))}
+              </SortableContext>
+            </DndContext>
           </div>
-          <div className="flex">Antrian Maimai Hari ini</div>
-          <div className="flex">Day, DD/MM/YYYY</div>
-      </header>
-      <div className="min-h-50vh mb-5 flex flex-col justify-center">
-        <div className="mb-5 flex flex-col items-center">
-          {names.length === 0 ? (
-            <span>There are no names</span>
-          ) : (
-            names.map((name, index) => (
-              <div
-                key={index}
-                className="name-item p-4 bg-amber-100 border-solid border-b-2 min-w-80 border-gray-800 w-1/2"
-              >
-                <span className="flex justify-center">{name}</span>
-              </div>
-            ))
-          )}
+          <div className='join flex justify-center mb-5'>
+            <input className='input input-bordered join-item' value={name} onChange={(e) => setName(e.target.value)} ></input>
+            <button
+              type='button'
+              className='btn btn-secondary join-item'
+              onClick={handleAddPlayer}
+            >
+              Queue
+            </button>
+          </div>
+          <div className='flex justify-center'>
+            <button className='btn w-52' onClick={()=>setShowModal(true)}>{text}</button>
+          </div>
         </div>
-        <div className="join flex justify-center">
-          <input className="input input-bordered join-item" value={name} onChange={(e) => setName(e.target.value)} ></input>
-          <button
-            type="button"
-            className="btn btn-secondary join-item"
-            onClick={handleAddPlayer}
-          >
-            Queue
-          </button>
-        </div>
-        <button className="btn" onClick={()=>document.getElementById('my_modal_2').showModal()}>{localStorage.getItem("username") == '' ?  'Insert your username' : 'Change your username '}</button>
+        <footer className='bg-maimai-background bg-center bg-cover bg-no-repeat bottom-0 w-full h-[31.25rem]'>
+        </footer>
       </div>
-      <footer className="bg-maimai-background bg-center bg-cover bg-no-repeat bottom-0 w-full h-[31.25rem]">
-          
-      </footer>
     </div>
+
   );
+  function handleDragEnd(event) {
+    const { active, over } = event;
+
+    if (active.id !== over.id) {
+      setNames((names) => {
+        const oldIndex = names.indexOf(active.id);
+        const newIndex = names.indexOf(over.id);
+
+        return arrayMove(names, oldIndex, newIndex);
+      });
+    }
+  }
 }
---
+++ src/components/SortableList/SortableList.jsx
@@ -0,0 +1,22 @@
+import { useSortable } from "@dnd-kit/sortable";
+import { CSS } from "@dnd-kit/utilities";
+
+export function SortableItem({ key, name, idx, itemSize}) {
+  const { attributes, listeners, setNodeRef, transform, transition } =
+    useSortable({ id: name });
+  const style = {
+    transform: CSS.Transform.toString(transform),
+    transition,
+  };
+
+  return (
+    <div ref={setNodeRef} style={style} {...attributes} {...listeners}>
+      <div
+        key={name}
+        className={`name-item p-4 bg-amber-100 border-solid border-b border-x min-w-80 border-gray-800 w-1/2 ${idx === 0 ? 'rounded-t-lg border-t' : ''} ${idx === itemSize-1 ? 'rounded-b-lg' : ''}`}
+      >
+        <span className="flex justify-center">{name+' '+idx}</span>
+      </div>
+    </div>
+  );
+}
